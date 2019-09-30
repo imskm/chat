5abeccf12716fadbc223f9f7a7a60a17b94e2ce2
Shek Muktar <im.skm07@gmail.com>
2019-09-30 11:16:09 +0530

    chat: chat_request_prepare function: cleaned the code

--- chat.c
+++ chat.c
@@ -4,6 +4,10 @@
 #include "easyio.h"
 #include "chat.h"
 
+static int prepare_request_for_message(struct request *req,
+		const char *cmd_bufp);
+
+
 int chat_command_handle(struct client *client)
 {
 	int 				nbytes, ret;
@@ -47,26 +51,21 @@ int	chat_request_prepare(struct request *req, const char *cmd_buf)
 	const char *cmd_bufp;
 
 	cmd_bufp = cmd_buf;
-	/* 1. Handle first phase of the command */
-	if (*cmd_bufp == '/') {
-		if ((cmd_index = command_handle(&cmd_bufp)) == -1) {
-			fprintf(stderr, "[!] Unknown command: %s\n", cmd_buf);
-			return -1;
-		}
-	} else { /* user has typed msg command without '/' (slash) */
-		/* If user is not associated with other user then he/she
-		 * can not send message without using /msg command */
-		if (!req->dest) {
-			fprintf(stderr, "[!] Can't send message, you are not associated\n");
-			return -1;
-		}
-		req->irc_cmd = commands[command_message_get_index()].irc_cmd;
-		req->body = strdup(cmd_bufp);
-		return 0;
+
+	/* 1. If user has not given command but typed something then it's special
+	 * case and this should be treated as message command */
+	if (*cmd_bufp != '/')
+		return prepare_request_for_message(req, cmd_bufp);
+
+	/* 1.1. Handle first phase of the command */
+	if ((cmd_index = command_handle(&cmd_bufp)) == -1) {
+		fprintf(stderr, "[!] Unknown command: %s\n", cmd_buf);
+		return -1;
 	}
 
-	cmd = &commands[cmd_index];
-	req->irc_cmd = cmd->irc_cmd;
+	cmd 			= &commands[cmd_index];
+	req->cmd 		= cmd->cmd;
+	req->irc_cmd 	= cmd->irc_cmd;
 	fprintf(stderr, "[*] Command found: %s\n", cmd->cmd);
 
 	/* 2. Since each command has its own unique structure and number of
@@ -90,23 +89,18 @@ int	chat_request_send(struct client *client, struct request *req)
 	 * len limit */
 
 	nbytes = sprintf(buf, ":%s %s", req->src, req->irc_cmd);
-	buf[nbytes] = 0;
 
 	/* Set destination (if any) */
-	if (req->dest) {
+	if (req->dest)
 		nbytes = sprintf(buf, "%s %s", buf, req->dest);
-		buf[nbytes] = 0;
-	}
 
 	/* Set params */
-	for (int i = 0; req->params[i] != NULL; i++) {
+	for (int i = 0; req->params[i] != NULL; i++)
 		nbytes = sprintf(buf, "%s %s", buf, req->params[i]);
-	}
 
 	/* Set message (if any) */
-	if (req->body) {
+	if (req->body)
 		nbytes = sprintf(buf, "%s :%s", buf, req->body);
-	}
 
 	/* Add CRLF at the end */
 	nbytes = sprintf(buf, "%s \r\n", buf);
@@ -121,3 +115,16 @@ int	chat_request_send(struct client *client, struct request *req)
 	return 0;
 }
 
+static int prepare_request_for_message(struct request *req,
+		const char *cmd_bufp)
+{
+	/* If user is not associated with other user then he/she
+	 * can not send message without using /msg command */
+	if (!req->dest) {
+		fprintf(stderr, "[!] Can't send message, you are not associated\n");
+		return -1;
+	}
+	req->irc_cmd = commands[command_message_get_index()].irc_cmd;
+	req->body = strdup(cmd_bufp);
+	return 0;
+}
