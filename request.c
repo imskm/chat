85c737bd214fffc18eec92bfdb9f5ae560d4cd50
Shek Muktar <im.skm07@gmail.com>
2019-10-01 00:48:02 +0530

    request: added few new function definition

--- request.c
+++ request.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "request.h"
 
@@ -21,8 +23,35 @@ int request_param_get(struct request *req, int index)
 	return 0;
 }
 
+int request_body_set(struct request *req, char *body)
+{
+	if (body == NULL)
+		return -1;
+	req->body = strdup(body);
+
+	return 0;
+}
+
+int request_dest_set(struct request *req, char *dest)
+{
+	if (dest == NULL)
+		return -1;
+	req->dest = strdup(dest);
+
+	return 0;
+}
+
 int request_cleanup(struct request *req)
 {
+	if (req->dest) free((void *)req->dest);
+	for (int i = 0; req->params[i] && i < REQUEST_MAX_PARAMS; i++) {
+		free(req->params[i]);
+		req->params[i] = NULL;
+	}
+	if (req->body) free(req->body);
+
+	req->dest = NULL;
+	req->body = NULL;
 
 	return 0;
 }
