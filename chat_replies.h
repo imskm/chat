3ee95dd0b5a61c73e12dc299e9a7dac0608e745f
Shek Muktar <im.skm07@gmail.com>
2019-10-03 18:23:37 +0530

    numeric replies: added two header file that contains error and reply
    Added two header files to store errors and replies codes
    Numeric replies of server is stored in chat_errors and chat_replies
    header file as symbolic constants of error and replies

---
+++ chat_replies.h
@@ -0,0 +1,48 @@
+#ifndef __CHAT_REPLIES_H
+#define __CHAT_REPLIES_H
+
+#define RPL_NONE 300     
+#define RPL_USERHOST 302     
+#define RPL_ISON 303     
+#define RPL_AWAY 301     
+#define RPL_UNAWAY 305     
+#define RPL_NOWAWAY 306     
+#define RPL_WHOISUSER 311     
+#define RPL_WHOISSERVER 312     
+#define RPL_WHOISOPERATOR 313     
+#define RPL_WHOISIDLE 317     
+#define RPL_ENDOFWHOIS 318     
+#define RPL_WHOISCHANNELS 319     
+#define RPL_WHOWASUSER 314     
+#define RPL_ENDOFWHOWAS 369     
+#define RPL_LISTSTART 321     
+#define RPL_LIST 322     
+#define RPL_LISTEND 323     
+#define RPL_CHANNELMODEIS 324     
+#define RPL_NOTOPIC 331     
+#define RPL_TOPIC 332     
+#define RPL_INVITING 341     
+#define RPL_SUMMONING 342     
+#define RPL_VERSION 351     
+#define RPL_WHOREPLY 352     
+#define RPL_ENDOFWHO 315     
+#define RPL_NAMREPLY 353     
+#define RPL_ENDOFNAMES 366     
+#define RPL_LINKS 364     
+#define RPL_ENDOFLINKS 365     
+#define RPL_BANLIST 367     
+#define RPL_ENDOFBANLIST 368     
+#define RPL_INFO 371     
+#define RPL_ENDOFINFO 374     
+#define RPL_MOTDSTART 375     
+#define RPL_MOTD 372     
+#define RPL_ENDOFMOTD 376     
+#define RPL_YOUREOPER 381     
+#define RPL_REHASHING 382     
+#define RPL_TIME 391     
+#define RPL_USERSSTART 392     
+#define RPL_USERS 393     
+#define RPL_ENDOFUSERS 394     
+#define RPL_NOUSERS 395     
+
+#endif
