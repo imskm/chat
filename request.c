79a9ddba7051d1d631b9e7bafd7adebd5e984f2d
Shek Muktar <im.skm07@gmail.com>
2019-10-03 18:31:38 +0530

    request c: added new functions to handle client request and more
    Added few new functions and request command handling function for
    handling client's request.
    Also fixed a conflicting type bug in request_dest_set function and
    replaced request.h include with chat.h

--- request.c
+++ request.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "request.h"
+#include "chat.h"
 
 int request_param_set(struct request *req, char *param)
 {
@@ -32,7 +32,7 @@ int request_body_set(struct request *req, char *body)
 	return 0;
 }
 
-int request_dest_set(struct request *req, char *dest)
+int request_dest_set(struct request *req, const char *dest)
 {
 	if (dest == NULL)
 		return -1;
@@ -44,6 +44,7 @@ int request_dest_set(struct request *req, char *dest)
 int request_cleanup(struct request *req)
 {
 	if (req->dest) free((void *)req->dest);
+	/* req->nick should not be freed */
 	for (int i = 0; req->params[i] && i < REQUEST_MAX_PARAMS; i++) {
 		free(req->params[i]);
 		req->params[i] = NULL;
@@ -52,6 +53,97 @@ int request_cleanup(struct request *req)
 
 	req->dest = NULL;
 	req->body = NULL;
+
+	return 0;
+}
+
+
+int request_handle(struct request *req, struct collection *collection)
+{
+	char *bufp = strdup(collection->buf);
+	char *parts[3];
+	int n, ret;
+
+	ret = -1;
+
+	/* Find second param from the buf */
+	if((n = str_split(bufp, " ", parts, 3)) != 2) 
+		goto cleanup;
+
+	str_trim(parts[0]), str_trim(parts[1]);
+
+	/* Check for empty command
+	 * parts[0] = :nick,  parts[1] = COMMAND */
+	if (strlen(parts[0]) <= 2 || strlen(parts[1]) <= 1)
+		goto cleanup;
+
+	for (int i = 0; commands[i].cmd; i++) {
+		if (strcmp(parts[1], commands[i].irc_cmd) == 0) {
+			ret = i;
+			goto cleanup;
+		}
+	}
+
+cleanup:
+	free(bufp);
+
+	return ret;
+}
+
+int request_handle_join(struct request *req, struct collection *collection)
+{
+	int n, ret = 0;
+	char *parts[4] = {0}, *p;
+
+	p = strdup(collection->buf);
+
+	if ((n = str_split(p, " ", parts, 4)) != 3) {
+		req->status = ERR_NEEDMOREPARAMS;
+		goto cleanup;
+	}
+
+	/* TODO this function must only deal with channel joining and not user
+	 * joining with other user. But for now I am implementing user chatting */
+
+	/* If argument for JOIN command is not given then return error */
+	if (chat_validate_nick(parts[2]) == false) {
+		req->status = ERR_NOSUCHNICK;
+		goto cleanup;
+	}
+
+	request_dest_set(req, parts[2]);
+	req->status = RPL_TOPIC;
+
+cleanup:
+	free(p);
+
+	return ret;
+}
+
+int request_handle_msg(struct request *req, struct collection *collection)
+{
+	puts("request_handle_msg");
+
+	return 0;
+}
+
+int request_handle_names(struct request *req, struct collection *collection)
+{
+	puts("request_handle_names");
+
+	return 0;
+}
+
+int request_handle_nick(struct request *req, struct collection *collection)
+{
+	puts("request_handle_nick");
+
+	return 0;
+}
+
+int request_handle_quit(struct request *req, struct collection *collection)
+{
+	puts("request_handle_quit");
 
 	return 0;
 }
