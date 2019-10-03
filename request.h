799aa92c1f9ccf7355f36bb9efe7aee05e421bb8
Shek Muktar <im.skm07@gmail.com>
2019-10-03 18:33:44 +0530

    request h: added new function prototypes and fixed a type mismatch bug

--- request.h
+++ request.h
@@ -5,8 +5,8 @@
 
 struct request {
 	int   type;
-	const char *src;   	/* nick of source  */
-	const char *dest;  	/* nick of destination  */
+	struct client *src; /* source (from which client request came) */
+	const char *dest;  	/* nick of destination */
 	const char *cmd;   	/* name of command (client side command) */
 	const char *irc_cmd;/* name of command (IRC protocol command) */
 	/* parameters of command message (NULL terminated) */
@@ -15,10 +15,20 @@ struct request {
 	int   status;		/* will be used for response */
 };
 
+
 int request_param_set(struct request *req, char *param);
 int request_param_get(struct request *req, int index);
 int request_cleanup(struct request *req);
 
 int request_body_set(struct request *req, char *body);
-int request_dest_set(struct request *req, char *dest);
+int request_dest_set(struct request *req, const char *dest);
+
+int request_handle(struct request *req, struct collection *collection);
+
+int request_handle_join(struct request *req, struct collection *collection);
+int request_handle_msg(struct request *req, struct collection *collection);
+int request_handle_names(struct request *req, struct collection *collection);
+int request_handle_nick(struct request *req, struct collection *collection);
+int request_handle_quit(struct request *req, struct collection *collection);
+
 #endif
