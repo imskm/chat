defcc9de6e3f098bdbbe3c9037862419513ff6de
Shek Muktar <im.skm07@gmail.com>
2019-10-09 13:23:42 +0530

    response: fixed a bug and added three new functions
    response_send_err forgot to add error code in response message
    Implemented response_send_rpl_none, response_send_rpl_join and
    response_send_rpl_names functions

--- response.c
+++ response.c
@@ -27,12 +27,17 @@ int response_send(int sockfd, const char *buf, size_t size)
 
 int response_send_err(struct request *req, struct collection *col)
 {
+	unsigned char buf[BUFFSIZE];
 	const struct response *res;
+	int nbytes;
+
 	res = &responses[chat_calc_reply_index(req->status)];
 
 	prepare_error_message(req, res, col);
 
-	return response_send(req->src->fd, req->body, strlen(req->body));
+	nbytes = sprintf(buf, "%d %s", req->status, req->body);
+
+	return response_send(req->src->fd, buf, nbytes);
 }
 
 void prepare_error_message(struct request *req, const struct response *res,
@@ -74,6 +79,19 @@ int response_send_msg(struct request *req, struct collection *col)
 	return 0;
 }
 
+int response_send_rpl_none(struct request *req, struct collection *col)
+{
+	unsigned char buf[BUFFSIZE];
+	int nbytes;
+
+	if (!req->body)
+		req->body = strdup("Action performed");
+
+	nbytes = sprintf(buf, "%d :%s", req->status, req->body);
+
+	return response_send(req->src->fd, buf, nbytes);
+}
+
 int response_send_rpl_join(struct request *req, struct collection *col)
 {
 	int i;
@@ -85,7 +103,20 @@ int response_send_rpl_join(struct request *req, struct collection *col)
 	if ((i = chat_find_nick(col->clients, req->dest)) == -1) {
 		req->status = ERR_NOSUCHNICK;
 		return response_send_err(req, col);
+	} else if (i == col->index) {
+		sprintf(buf, "%d :You can't associate your self", RPL_NONE);
+		return response_send(req->src->fd, buf, strlen(buf));
 	}
+
+	col->clients->clients[col->index]->partner = col->clients->clients[i];
+	col->clients->clients[i]->partner = col->clients->clients[col->index];
+
+	/* Notify the connected client */
+	sprintf(buf, "%d :<%s> has requested association", RPL_NONE,
+			req->src->nick);
+	response_send(col->clients->clients[col->index]->partner->fd, buf,
+			strlen(buf));
+
 	sprintf(buf, "%d :You have joined successfully!", RPL_NONE);
 
 	return response_send(req->src->fd, buf, strlen(buf));
@@ -93,6 +124,17 @@ int response_send_rpl_join(struct request *req, struct collection *col)
 
 int response_send_rpl_names(struct request *req, struct collection *col)
 {
+	char buf[BUFFSIZE];
+
+	sprintf(buf, "%d ", RPL_NAMREPLY);
+	chat_serialize_nick(col->clients, buf + 4, sizeof(buf) - 4);
+
+	/* TODO Handle the case when buf is full but more nick is remaining */
+
+	response_send(req->src->fd, buf, strlen(buf));
+	sprintf(buf, "%d", RPL_ENDOFNAMES);
+
+	return response_send(req->src->fd, buf, strlen(buf));
 }
 
 int response_send_rpl_nick(struct request *req, struct collection *col)
